4e9186c6f853a3d570afe8d388ee14b5bbe3e8db
Lukas Hermann <lukher6@web.de>
2018-12-02 20:40:09 +0800

    Initial commit with dummy

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * List of streams to check regularely
+ */
+define('STREAMS', [
+    "joel-rtmp" => "rtmp://streamer1.streamhost.org/salive/joelmediatv",
+    "joel-hls" => "https://streamer1.streamhost.org:443/salive/joelmediatv/playlist.m3u8"
+]);
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * DUMMY
+ */
+$path = explode('/', $_SERVER['PATH_INFO']);
+$id = end($path);
+
+class Check {
+    public $live = false;
+    public $last_check = 0;
+
+    function __construct() {
+        $this->last_check = time();
+    }
+}
+
+$return = new Check();
+
+switch ($id) {
+    case 'joelmedia':
+        $return->live = true;
+        break;
+    default:
+        # code...
+        break;
+}
+
+print( json_encode($return) );
+
+
+
+
+// $exec = "timeout 10s /usr/local/bin/ffprobe -v quiet -print_format json -show_format -show_streams -i " . $stream . " 2>&1";
+// $temp = shell_exec ( $exec );
+// $json = json_decode( $temp );
+
+// timeout 10s ffprobe -v quiet -print_format json -show_format -show_streams -i https://streamer1.streamhost.org:443/salive/joelmediatv/playlist.m3u8
+// timeout 10s ffprobe -v quiet -print_format json -show_format -show_streams -i rtmp://streamer1.streamhost.org/salive/joelmediatv
+// timeout 10s ffprobe -v quiet -print_format json -show_format -show_streams -i rtmp://live.stream.joelmediatv.de:1935/live/joelmedia
+// timeout 10s ffprobe -v quiet -print_format json -show_format -show_streams -i https://streamer1.streamhost.org/salive/lctvde/playlist.m3u8
+// timeout 10s ffprobe -v quiet -print_format json -show_format -show_streams -i rtmp://streamer1.streamhost.org/salive/lctvde
