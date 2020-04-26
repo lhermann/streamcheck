947eb446cad0944064f284ef9c5cc59bff3f245a
Lukas Hermann <lukher6@web.de>
2020-04-26 08:43:25 +0200

    chore: update server setup

--- router.php
+++ router.php
@@ -45,50 +45,9 @@ function getRoot () {
 }
 
 function getApi () {
-  require __ROOT__ . '/api/index.json';
+  require __ROOT__ . '/inc/index.json';
 }
 
 function getApiV1 () {
-  require __ROOT__ . '/api/v1.json';
+  require __ROOT__ . '/inc/v1.json';
 }
-
-
-// Router
-// switch (array_shift($route)) {
-//   case '':
-//     return require __DIR__ . '/info.php';
-//   case 'api':
-//     return routeApi($route);
-//   default:
-//     return route404();
-// }
-
-
-// Router Functions
-// function routeApi ($route) {
-//   header('Content-Type: application/json');
-
-//   switch ($route) {
-//     case ['v1']:
-//       return require __DIR__ . '/api/v1/index.php';
-//     case ['v1', 'check']:
-//       return require __DIR__ . '/check.php';
-//     case ['v2']:
-//       return require __DIR__ . '/api/v2/index.json';
-//     case ['v2', 'auth']:
-//       return require __DIR__ . '/api/v2/auth.php';
-//     case ['v2', 'auth', 'callback']:
-//       return require __DIR__ . '/api/v2/auth_callback.php';
-//     case ['v2', 'check']:
-//       return require __DIR__ . '/api/v2/check.php';
-//     case ['v2', 'status']:
-//       return require __DIR__ . '/api/v2/status.php';
-//     default:
-//       return require __DIR__ . '/api/index.json';
-//   }
-// }
-
-// function route404 () {
-//   http_response_code(404);
-//   print("404");
-// }
