e2da0cd1e3498ed510955a00afd409612bd80766
Lukas Hermann <lukher6@web.de>
2020-05-02 23:02:37 +0200

    feat: redirect google api auth back to ui page

--- controller/AuthCtrl.php
+++ controller/AuthCtrl.php
@@ -27,7 +27,7 @@ class AuthCtrl {
       $config = $filtered_list[0];
       $client = new Google_API($config);
       $client->authenticate($_GET['code']);
-      header('Location: //' . $_SERVER['HTTP_HOST'] . '/api/v1/auth');
+      header('Location: //' . $_SERVER['HTTP_HOST'] . '/ui/');
     } else {
       throw new Exception('The session state did not match.');
     }
