fddc0b156ff7ec3ed751983462e02f6768c2d420
Lukas Hermann <lukher6@web.de>
2020-05-02 23:00:40 +0200

    fix: change type to password

--- ui/src/components/CredentialsForm.vue
+++ ui/src/components/CredentialsForm.vue
@@ -17,7 +17,7 @@
       </label>
       <input
         id="form-pass"
-        type="text"
+        type="password"
         class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
         v-model="internalPassword"
       >
